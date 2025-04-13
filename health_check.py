5ada29771abc63e64d363c2570cef1cc39951fe9
@om56577 <116886444+Nandeshwarom555@users.noreply.github.com>
2025-04-13 12:34:44 +0530

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/health', methods=['GET'])
+@app.route('/health')
 def health_check():
-    return "OK", 200  # Return a success response for health check
+    return 'OK', 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
